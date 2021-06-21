e4ffab08b0a27604c5f1ee2428ff84a11beb7ce9
belov <belovdenis777@gmail.com>
2021-06-21 22:56:54 +0300

    Универсальный onChange для Input

--- src/components/App.js
+++ src/components/App.js
@@ -1,90 +1,108 @@
 import React from "react";
+import countries from "../data/countries"
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      username: "",
-      password: "",
-      repeatPassword: ""
+        this.state = {
+            username: "",
+            password: "",
+            repeatPassword: "",
+            country: '1'
+        };
+    }
+
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    onSubmit = event => {
+        event.preventDefault();
+        // console.log("refs", this.username.value, this.password.value);
+        console.log("submit", this.state);
     };
-  }
+    getOptionItems = items => {
+        return items.map(items => (
+            <option
+                key={items.id}
+                value={items.id}
+            >
+                {items.name}
+            </option>
+        ))
+    }
 
-  onChangeUsername = event => {
-    this.setState({
-      username: event.target.value
-    });
-  };
+    render() {
+        // console.log(this);
+        return (
+            <div className="form-container card">
+                <form className="form card-body">
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter username"
+                            ref={node => (this.username = node)}
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter password"
+                            ref={node => (this.password = node)}
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Repeat password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter repeat password"
+                            ref={node => (this.repeatPassword = node)}
+                            name="repeatPassword"
+                            value={this.state.repeatPassword}
+                            onChange={this.onChange}
+                        />
+                    </div>
+                    {/*<div className='form-group'>*/}
+                    {/*    <label htmlFor="country">Country</label>*/}
+                    {/*    */}
+                    {/*</div>*/}
+                    <div className='form-group'>
+                        <label htmlFor="country">Country</label>
+                        <select
+                            className="form-select"
+                            value={this.state.country}
+                            name="country"
+                            onChange={this.onChange}
+                        >
+                            {this.getOptionItems(countries)}
+                        </select>
 
-  onChangePassword = event => {
-    this.setState({
-      password: event.target.value
-    });
-  };
+                    </div>
 
-  onChangeRepeatPassword = event => {
-    this.setState({
-      repeatPassword: event.target.value
-    });
-  };
 
-  onSubmit = event => {
-    event.preventDefault();
-    // console.log("refs", this.username.value, this.password.value);
-    console.log("submit", this.state);
-  };
-
-  render() {
-    // console.log(this);
-    return (
-      <div className="form-container card">
-        <form className="form card-body">
-          <div className="form-group">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter username"
-              ref={node => (this.username = node)}
-              name="username"
-              value={this.state.username}
-              onChange={this.onChangeUsername}
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter password"
-              ref={node => (this.password = node)}
-              name="password"
-              value={this.state.password}
-              onChange={this.onChangePassword}
-            />
-          </div>
-          <div className="form-group">
-            <label>Repeat password</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter repeat password"
-              ref={node => (this.repeatPassword = node)}
-              name="repeatPassword"
-              value={this.state.repeatPassword}
-              onChange={this.onChangeRepeatPassword}
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn btn-primary w-100"
-            onClick={this.onSubmit}
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
+                    <button
+                        type="submit"
+                        className="btn btn-primary w-100"
+                        onClick={this.onSubmit}
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
